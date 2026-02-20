7a5232e210e98256a4e20d148889a4b79adc9c03
Dušan Schnelly <d.schnelly@gmail.com>
2026-02-20 13:14:15 +0100

    Small tune

--- index.tsx
+++ index.tsx
@@ -1,3 +1,8 @@
+/// <reference types="vite/client" />
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// ... zbytek tvého kódu
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
